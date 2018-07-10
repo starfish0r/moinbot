b01419ff4c442504fa25655c365ab55504547fad
Christian Abraham <starfish0r@gmail.com>
2018-07-10 11:00:21 +0200

    copied stuff from the jbot example, made it gittable

---
+++ src/main/java/de/cabraham/moinbot/JBotApplication.java
@@ -0,0 +1,25 @@
+package de.cabraham.moinbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "de.cabraham.moinbot"})
+public class JBotApplication {
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    } 
+
+    /**
+     * Entry point of the application. Run this method to start the sample bots,
+     * but don't forget to add the correct tokens in application.properties file.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(JBotApplication.class, args);
+    }
+}
---
+++ src/main/java/de/cabraham/moinbot/MoinBot.java
@@ -0,0 +1,4 @@
+package de.cabraham.moinbot;
+
+public class MoinBot {
+}
